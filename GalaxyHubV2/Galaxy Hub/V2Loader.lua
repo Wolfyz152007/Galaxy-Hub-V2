00d518fba660231d692af31fafc7a0674ab6caee
Wolfyz152007 <108429796+Wolfyz152007@users.noreply.github.com>
2022-11-10 23:10:29 +0000

    Create V2Loader.lua

---
+++ GalaxyHubV2/Galaxy Hub/V2Loader.lua
@@ -0,0 +1,3 @@
+if game.PlaceId == 155615604 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Wolfyz152007/Galaxy-Hub-V2/main/GalaxyHubV2/Galaxy%20Hub/V2Games/Prison%20Life.lua"))()
+end
